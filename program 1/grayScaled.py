a42c09962194c84faa9e980686f120f86dc21faa
hussein69 <husseinhafez690@gmail.com>
2021-03-08 12:03:12 +0200

    initial commit

--- program 1/grayScaled.py
+++ program 1/grayScaled.py
@@ -2,7 +2,12 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread("gabenweed.jpg")
-cv2.imshow("gaben", img)
+img1 = cv2.imread("gabenweed.jpg", 0)
+cv2.imwrite("grayScaled.png", img1)
+img2 = cv2.imread("grayScaled.png")
+cv2.rectangle(img2, (250,200), (350,300), (0,0,255), -1)
+
+
+cv2.imshow("image", img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
