6986342656159bdd2e89edfd7d32dbd5ba19a295
Robotwizard <49511689+hussein69@users.noreply.github.com>
2021-03-09 10:31:12 +0200

    Update edgeDetection.py

--- program 3/edgeDetection.py
+++ program 3/edgeDetection.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 img = cv2.imread("tomb-raider-cover.jpg")
-edges = cv2.Canny(img, 200, 300)
+edges = cv2.Canny(img, 300, 300)
 cv2.imshow("image", img)
 cv2.imshow("edges", edges)
 cv2.waitKey(0)
